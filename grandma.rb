74f45283279cb7b39f6bd2818970b6054350b946
David Strauch <dmaster18@gmail.com>
2019-07-21 19:19:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma (phrase)  do
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
   else
   end
 end
