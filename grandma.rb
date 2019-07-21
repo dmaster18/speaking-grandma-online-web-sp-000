d2b55fab3950f9e52673a157c52537fbca2ef607
David Strauch <dmaster18@gmail.com>
2019-07-21 19:20:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma (phrase)  do
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif
+  elsif phrase = "I LOVE YOU GRANDMA!"
   end
 end
 
